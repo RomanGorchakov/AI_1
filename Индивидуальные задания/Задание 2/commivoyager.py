cf79728cae43e9829fabbb845cbe335b96a27e88
RomanGorchakov <144220160+RomanGorchakov@users.noreply.github.com>
2024-10-21 07:26:12 +0000

    Graphs

---
+++ Индивидуальные задания/Задание 2/commivoyager.py
@@ -0,0 +1,102 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Graph:
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.size = len(vertices)
+        self.edges = [[None for _ in range(self.size)] for _ in range(self.size)]
+
+    def add_edge(self, value1, value2, weight):
+        row = self.vertices.index(value1)
+        column = self.vertices.index(value2)
+        
+        self.edges[row][column] = weight
+        self.edges[column][row] = weight
+
+class Route:
+    def __init__(self, vertices, weight):
+        self.vertices = vertices
+        self.weight = weight
+
+    def add(self, vertex, inc_weight):
+        next_vertices = self.vertices + [vertex]
+        next_weight = self.weight + inc_weight
+        
+        return Route(next_vertices, next_weight)
+
+def tsp(self):
+    seen = set()
+    min_route = None
+
+    def bruteforce(i, current):
+        nonlocal min_route
+        seen.add(i)
+
+        if len(seen) == self.size:
+            weight = self.edges[i][0]
+            if weight is not None:
+                route = current.add(self.vertices[0], weight)
+                if min_route is None or min_route.weight > route.weight:
+                    min_route = route
+        else:
+            for j in range(len(self.edges[i])):
+                weight = self.edges[i][j]
+                if weight is not None and j not in seen:
+                    route = current.add(self.vertices[j], weight)
+                    bruteforce(j, route)
+
+        seen.remove(i)
+
+    bruteforce(0, Route([self.vertices[0]], 0))
+    return min_route
+
+# Добавляем функцию TSP к классу Graph
+Graph.tsp = tsp
+
+# Пример использования
+if __name__ == '__main__':
+    graph = Graph(['Новоалександровск', 'Присадовый', 'Ударный', 'Краснодарский', \
+    'Красночервонный', 'Южный', 'Расшеватская', 'Славенский', 'Озёрный', 'Темижбекский', \
+    'Краснокубанский'])
+    graph.add_edge('Новоалександровск', 'Присадовый', 10)
+    graph.add_edge('Новоалександровск', 'Ударный', 15)
+    graph.add_edge('Новоалександровск', 'Краснодарский', 13)
+    graph.add_edge('Новоалександровск', 'Красночервонный', 11)
+    graph.add_edge('Новоалександровск', 'Южный', 9)
+    graph.add_edge('Новоалександровск', 'Расшеватская', 19)
+    graph.add_edge('Присадовый', 'Новоалександровск', 10)
+    graph.add_edge('Присадовый', 'Ударный', 5)
+    graph.add_edge('Ударный', 'Новоалександровск', 15)
+    graph.add_edge('Ударный', 'Присадовый', 5)
+    graph.add_edge('Ударный', 'Краснодарский', 9)
+    graph.add_edge('Краснодарский', 'Новоалександровск', 13)
+    graph.add_edge('Краснодарский', 'Ударный', 9)
+    graph.add_edge('Краснодарский', 'Красночервонный', 12)
+    graph.add_edge('Красночервонный', 'Новоалександровск', 11)
+    graph.add_edge('Красночервонный', 'Краснодарский', 12)
+    graph.add_edge('Красночервонный', 'Южный', 12)
+    graph.add_edge('Южный', 'Новоалександровск', 9)
+    graph.add_edge('Южный', 'Красночервонный', 12)
+    graph.add_edge('Южный', 'Расшеватская', 15)
+    graph.add_edge('Южный', 'Славенский', 8)
+    graph.add_edge('Южный', 'Темижбекский', 8)
+    graph.add_edge('Расшеватская', 'Новоалександровск', 19)
+    graph.add_edge('Расшеватская', 'Южный', 15)
+    graph.add_edge('Расшеватская', 'Славенский', 16)
+    graph.add_edge('Славенский', 'Южный', 8)
+    graph.add_edge('Славенский', 'Расшеватская', 16)
+    graph.add_edge('Славенский', 'Озёрный', 3)
+    graph.add_edge('Славенский', 'Темижбекский', 13)
+    graph.add_edge('Озёрный', 'Славенский', 3)
+    graph.add_edge('Озёрный', 'Темижбекский', 5)
+    graph.add_edge('Озёрный', 'Краснокубанский', 5)
+    graph.add_edge('Темижбекский', 'Южный', 8)
+    graph.add_edge('Темижбекский', 'Славенский', 13)
+    graph.add_edge('Темижбекский', 'Озёрный', 5)
+    graph.add_edge('Темижбекский', 'Краснокубанский', 5)
+    graph.add_edge('Краснокубанский', 'Озёрный', 5)
+    graph.add_edge('Краснокубанский', 'Темижбекский', 5)
+
+    result = graph.tsp()
+    print(result.vertices, result.weight)
---
+++ Пример/example.py
@@ -0,0 +1,95 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+import heapq
+import math
+import sys
+from collections import defaultdict, deque, Counter
+from itertools import combinations
+
+
+class Problem:
+    def __init__(self, initial=None, goal=None, **kwds):
+        self.__dict__.update(initial=initial, goal=goal, **kwds)
+
+    def actions(self, state):
+        raise NotImplementedError
+
+    def result(self, state, action):
+        raise NotImplementedError
+
+    def is_goal(self, state):
+        return state == self.goal
+
+    def action_cost(self, s, a, s1):
+        return 1
+
+    def h(self, node):
+        return 0
+
+    def __str__(self):
+        return '{}({!r}, {!r})'.format(
+            type(self).__name__, self.initial, self.goal)
+
+class Node:
+    def __init__(self, state, parent=None, action=None, path_cost=0):
+        self.__dict__.update(state=state, parent=parent, action=action, \
+path_cost=path_cost)
+
+    def __repr__(self):
+        return '<{}>'.format(self.state)
+
+    def __len__(self):
+        return 0 if self.parent is None else (1 + len(self.parent))
+
+    def __lt__(self, other):
+        return self.path_cost < other.path_cost
+
+failure = Node('failure', path_cost=math.inf)
+cutoff = Node('cutoff', path_cost=math.inf)
+
+def expand(problem, node):
+    s = node.state
+    for action in problem.actions(s):
+        s1 = problem.result(s, action)
+        cost = node.path_cost + problem.action_cost(s, action, s1)
+        yield Node(s1, node, action, cost)
+
+def path_actions(node):
+    if node.parent is None:
+        return []
+    return path_actions(node.parent) + [node.action]
+
+def path_states(node):
+    if node in (cutoff, failure, None):
+        return []
+    return path_states(node.parent) + [node.state]
+
+FIFOQueue = deque
+LIFOQueue = list
+
+class PriorityQueue:
+    def __init__(self, items=(), key=lambda x: x):
+        self.key = key
+        self.items = [] # a heap of (score, item) pairs
+        for item in items:
+            self.add(item)
+
+    def add(self, item):
+        pair = (self.key(item), item)
+        heapq.heappush(self.items, pair)
+
+    def pop(self):
+        return heapq.heappop(self.items)[1]
+
+    def top(self):
+        return self.items[0][1]
+
+    def __len__(self):
+        return len(self.items)
+
+
+if __name__ == '__main__':
+    p1 = Problem()
+    p1.actions(0)
